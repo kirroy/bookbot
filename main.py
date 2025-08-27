c3e3d4cbc983f309096e302631f7018541da33ed
kirroy <yorrik217@gmail.com>
2025-08-27 13:39:09 +0200

    2

--- main.py
+++ main.py
@@ -1,16 +1,22 @@
 
-from stats import get_num_words, get_num_single
-
+from stats import get_num_single, get_num_words, sorting, dics_list, dic_einzel
 count_dic = {}
+zahl = 0
 def get_book_text(books_path):
     with open(books_path) as f:
         return f.read()
 
+
 def main():
     text = get_book_text("books/frankenstein.txt")
     words = get_num_words(text)
-    bstn = get_num_single(words)
-    print(f"Number of words: {len(words)}")
-    print(f"First word in lowercase: {bstn}")
-
+    dic = get_num_single(text)
+    liste = dics_list(dic)
+    print("============ BOOKBOT ============")
+    print("Analyzing book found at books/frankenstein.txt...")
+    print("----------- Word Count ----------")
+    print(f"Found {len(words)} total words")
+    print("----------- Character Count -----------")
+    dic_einzel(liste)
+    print("============= END ===============")
 main()
--- stats.py
+++ stats.py
@@ -1,11 +1,29 @@
 
+def get_num_single(text):
+    dic = {}
+    for key in text:
+        low = key.lower()
+        if (low in dic) and (low.isalpha()):
+            dic[low] += 1
+        elif low not in dic and low.isalpha():
+            dic[low] = 1
+    return dic
+        
+    
 def get_num_words(text):
      words = text.split()
      return words
 
-def get_num_single(words):
-    for word in words:
-        bstn = word.lower()
-    return bstn
+def dics_list(dic):
+    liste = []
+    for key in dic:
+        liste.append({"char": key, "num": dic[key]})
+    liste.sort(reverse = True, key=sorting)
+    return liste
+
+def dic_einzel(liste):
+    for eintrag in liste:
+        print(f"{eintrag['char']}: {eintrag['num']}")
         
-        
+def sorting(liste):
+    return liste["num"]
